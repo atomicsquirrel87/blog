1ca5ff8ab4debcded6eaec31fae9064a82b45522
atomicsquirrel87 <domkes87@googlemail.com>
2018-03-22 09:43:40 +0100

    added user_id to article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307123136) do
+ActiveRecord::Schema.define(version: 20180322084106) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20180307123136) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
 end
